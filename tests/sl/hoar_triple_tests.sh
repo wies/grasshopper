5e815633ad31d9d9536cb91b263dd50b5ee72c5b
zufferey <zufferey@88015cbe-a304-4c29-8203-3a0fa617ae4c>
2012-08-23 13:08:58 +0000

    a script to run the hoar triple examples

---
+++ tests/sl/hoar_triple_tests.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+#that script should be run in the project root folder, i.e. ../../
+
+z3_instantiate () {
+    echo $1 
+    time ./interpolate.native -sl2 -z3q tests/sl/$1
+}
+
+eager_instantiate () {
+    echo $1 
+    time ./interpolate.native -sl2 tests/sl/$1
+}
+
+z3_instantiate traverse_1
+z3_instantiate traverse_2
+z3_instantiate traverse_3
+
+z3_instantiate insert_1
+z3_instantiate insert_2
+eager_instantiate insert_3
+z3_instantiate insert_4
+
+z3_instantiate remove_1
+z3_instantiate remove_2
+eager_instantiate remove_3
+
+z3_instantiate concat_1
+eager_instantiate concat_2
+z3_instantiate concat_3
+
+z3_instantiate reverse_1
+z3_instantiate reverse_2
+z3_instantiate reverse_3
