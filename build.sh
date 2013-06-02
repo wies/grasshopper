13b0db54d532e181ecd6bfadba107a08e2bf623b
wies <wies@88015cbe-a304-4c29-8203-3a0fa617ae4c>
2013-06-02 04:16:07 +0000

    move things around

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-DIRS="-Is src/util,src/smtlib,src/programs,src/frontends/spl,src/prover,src/frontends/grass,src/sl,src/analysis,src/main"
+DIRS="-Is src/util,src/form,src/smtlib,src/programs,src/frontends/spl,src/prover,src/frontends/grass,src/sl,src/analysis,src/main"
 TARGET="src/main/grassprover src/main/slprover src/main/analyzer src/main/grasshopper "
 FLAGS="-cflag -g -lflag -g -libs unix,str $DIRS"
 OCAMLBUILD=ocamlbuild
