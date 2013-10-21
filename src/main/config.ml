42d36f4a943a4a8af6d540d67eac6c8f9269b455
wies <wies@88015cbe-a304-4c29-8203-3a0fa617ae4c>
2013-10-21 02:32:28 +0000

    improve description of command line options

--- src/main/config.ml
+++ src/main/config.ml
@@ -34,22 +34,22 @@ let optSelfFrame = ref true
 let experimental = ref false
 
 let cmd_options =
-  [("-v", Arg.Unit Debug.set_debug, " Display verbose messages");
+  [("-model", Arg.Set_string model_file, "<file>  Produce counterexample model for the first failing verification condition");
    ("-dumpghp", Arg.Set_int dump_ghp, "<num>  Print intermediate program after specified simplification stage (num=0,1,2,3)");
+   ("-dumpvcs", Arg.Set dump_smt_queries, " Generate SMT-LIB 2 files for all verification conditions");
    ("-noverify", Arg.Clear verify, " Do not check the generated verification conditions");
    ("-robust", Arg.Set robust, " Continue even if some verification condition cannot be checked");
-   ("-stats", Arg.Set print_stats, " Print statistics");
-   ("-model", Arg.Set_string model_file, "<file>  Produce counterexample model for the first failing verification condition");
-   ("-noreach", Arg.Clear with_reach_axioms, " Omit axioms for reachability predicates");
-   ("-noep", Arg.Clear with_ep, " Omit entry points");
-   ("-elimsets", Arg.Clear keep_sets, " Eliminate sets in reduction to GRASS");
+   ("-smtsolver", Arg.Set_string smtsolver, " Choose SMT solver (Z3, CVC4, MathSAT)");
    ("-smtsets", Arg.Set backend_solver_has_set_theory, " Use set theory of SMT solver to encode sets");
    ("-smtarrays", Arg.Set encode_fields_as_arrays, " Use array theory of SMT solver to encode fields");
+   ("-noreach", Arg.Clear with_reach_axioms, " Omit axioms for reachability predicates");
+   ("-noep", Arg.Clear with_ep, " Omit entry points");
+   ("-nosets", Arg.Clear keep_sets, " Eliminate sets in reduction to GRASS");
    ("-noinst", Arg.Clear instantiate, " Let the SMT solver deal with the quantifiers without prior instantiation");
    ("-nostratify", Arg.Clear stratify, " Instantiate quantifiers that satisfy stratified sort restrictions");
-   ("-dumpvcs", Arg.Set dump_smt_queries, " Generate SMT-LIB 2 files for all verification conditions");
-   ("-smtsolver", Arg.Set_string smtsolver, " Choose SMT solver (Z3, CVC4, MathSAT)");
    ("-noOptFieldMod", Arg.Clear optFieldMod, " Disable mod set analysis optimization for fields");
    ("-noOptSelfFrame", Arg.Clear optSelfFrame, " Disable generation of self-framing clauses for SL predicates");
-   ("-x", Arg.Set experimental, " Enable some experimental feature")
+   ("-stats", Arg.Set print_stats, " Print statistics");
+   ("-x", Arg.Set experimental, " Enable some experimental feature");
+   ("-v", Arg.Unit Debug.set_debug, " Display verbose messages");
   ]
