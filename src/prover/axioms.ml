0839ddbbb66122f47968ad42dd478e905057f6cf
wies <wies@cs.nyu.edu>
2014-12-12 13:47:14 -0500

    experimental reachability axioms

--- src/prover/axioms.ml
+++ src/prover/axioms.ml
@@ -147,9 +147,14 @@ let reach_axioms () =
   let btwn_trn3 = mk_or [mk_not (btwn loc1 loc2 loc3); mk_not (btwn loc1 loc4 loc2);
                          mk_and [btwn loc1 loc4 loc3; btwn loc4 loc2 loc3]]
   in
-  (*let btwn_trn4 = mk_or [mk_not (btwn loc1 loc2 loc3);
-                         mk_and [btwn loc1 loc2 loc2; btwn loc1 loc3 loc3]]
-  in*)
+  let btwn_trn4 = mk_or [mk_not (btwn loc1 loc2 loc3);
+                         mk_and [btwn loc1 loc2 loc2; btwn loc1 loc3 loc3;
+                                 btwn loc1 loc1 loc2; btwn loc1 loc1 loc3;
+                                 btwn loc2 loc3 loc3; btwn loc2 loc2 loc3]]
+  in
+  let btwn_sndw2 = mk_or [mk_not (btwn loc1 loc2 loc3); mk_not (btwn loc2 loc3 loc1);
+                          mk_and [mk_eq loc1 loc2; mk_eq loc2 loc3]]
+  in
   (**)
   if !with_reach_axioms then
     [mk_axiom "btwn_refl" btwn_refl; 
@@ -158,11 +163,13 @@ let reach_axioms () =
      mk_axiom "btwn_reach" btwn_reac;
      mk_axiom "btwn_sndw" btwn_sndw; 
      mk_axiom "btwn_ord1" btwn_ord1;
-     mk_axiom "btwn_ord2" btwn_ord2;
+     (*mk_axiom "btwn_ord2" btwn_ord2;*)
      mk_axiom "btwn_trn1" btwn_trn1; 
-     mk_axiom "btwn_trn2" btwn_trn2;
-     mk_axiom "btwn_trn3" btwn_trn3;
-     (*mk_axiom "btwn_trn4" btwn_trn4*)]
+     (*mk_axiom "btwn_trn2" btwn_trn2;
+     mk_axiom "btwn_trn3" btwn_trn3;*)
+     mk_axiom "btwn_trn4" btwn_trn4;
+     mk_axiom "btwn_snd2" btwn_sndw2;
+   ]
   else []
 
 (** Axioms for null *)
