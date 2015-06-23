cfb16216e0512da1e5c31178de4d03501c8c2335
Damien Zufferey <zufferey@csail.mit.edu>
2015-06-23 19:00:19 -0400

    can save model as html

--- src/main/vizmodel.ml
+++ src/main/vizmodel.ml
@@ -31,7 +31,10 @@ let vizmodel file =
   let model = parse_input file in
   let model_chan = open_out !Config.model_file in
   (* Model.print_model2 model;*)
-  Model.output_graph model_chan model Grass.TermSet.empty;
+  if Str.string_match (Str.regexp ".*\\.html$") !Config.model_file 0 then
+    Model.output_visjs model_chan (Model.complete model) Grass.TermSet.empty
+  else
+    Model.output_graph model_chan (Model.complete model) Grass.TermSet.empty;
   close_out model_chan  
 
 let _ =
