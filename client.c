f62cdde4e0c2c090c720c2d1f8119f39aae06f7b
JaneEyre446 <ramseyer@mit.edu>
2015-12-13 23:22:28 -0500

    got client server code to C, now working on implementing socket code.
    Signed-off-by: JaneEyre446 <ramseyer@mit.edu>

--- client.c
+++ client.c
@@ -4,6 +4,7 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <netinet/in.h>
 
 /*
  * Preloaded Code
@@ -57,52 +58,125 @@ SPLArray_generic* newSPLArray_generic(int size) {
   return a;
 }
 
+/*
+ * Structs
+ */
+struct SocketAddressIP4;
+struct SocketAddressIP6;
+
+typedef struct SocketAddressIP4 {
+  int sin4_addr;
+  int sin4_port;
+} SocketAddressIP4;
+
+typedef struct SocketAddressIP6 {
+  SPLArray_char* sin6_addr;
+  int sin6_flowinfo;
+  int sin6_port;
+  int sin6_scope_id;
+} SocketAddressIP6;
 
 /*
  * Procedures
  */
 int Main (SPLArray_char* args);
+bool bind4 (int fd, struct SocketAddressIP4* address);
+bool bind6 (int fd_1, struct SocketAddressIP6* address_1);
 int client (SPLArray_char* host, int mode, int version);
 SPLArray_char* constructPacket (int mode_1, int version_1);
 SPLArray_int* copy (SPLArray_int* a);
 SPLArray_char* copyByte (SPLArray_char* a_1);
-int gclose (int fd_1);
+int create_socket (int inet_type, int socket_type, int protocol);
+int gclose (int fd_4);
+struct SocketAddressIP4* get_address4 (SPLArray_char* node, SPLArray_char* service);
+struct SocketAddressIP6* get_address6 (SPLArray_char* node_1, SPLArray_char* service_1);
 int gopen (SPLArray_char* pathname, int flags_1);
-int gread (int fd_3, SPLArray_char* buffer_1);
-int greadOffset (int fd_4, SPLArray_char* buffer_2, int offset);
-int gwrite (int fd_5, SPLArray_char* buffer_3);
+int gread (int fd_6, SPLArray_char* buffer_1);
+int greadOffset (int fd_7, SPLArray_char* buffer_2, int offset);
+int gwrite (int fd_8, SPLArray_char* buffer_3);
 SPLArray_char* intToByteArray (int i_8);
+int udp_recv4 (int fd_9, SPLArray_char* msg, struct SocketAddressIP4* from);
+int udp_recv6 (int fd_10, SPLArray_char* msg_1, struct SocketAddressIP6* from_1);
+int udp_send4 (int fd_11, SPLArray_char* msg_2, int len, struct SocketAddressIP4* address_4);
+int udp_send6 (int fd_12, SPLArray_char* msg_3, int len_1, struct SocketAddressIP6* address_5);
 
 int Main (SPLArray_char* args) {
   int res;
+  if ((!((args->length) == 4))) {
+    return 1;
+  }
   res = client(args, 1, 1);
   return res;
 }
 
 int client (SPLArray_char* host, int mode, int version) {
   int time;
+  SPLArray_char* tmp_1;
+  SPLArray_char* tmp;
   int temp;
+  bool success_2;
+  int sent;
+  SPLArray_char* response;
+  int res_1;
+  int received;
+  SPLArray_char* port;
   SPLArray_char* packet;
   int flags;
-  int fd;
+  int fd_2;
+  int closed;
   SPLArray_char* buffer;
+  struct SocketAddressIP4* addr;
   
   flags = 0;
-  fd = gopen(host, flags);
+  fd_2 = create_socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
+  if ((fd_2 == (-1))) {
+    return 1;
+  }
+  tmp = newSPLArray_char( 4);
+  port = tmp;
+  (port->arr[0]) = ((char) 52);
+  (port->arr[1]) = ((char) 52);
+  (port->arr[2]) = ((char) 52);
+  (port->arr[3]) = ((char) 0);
+  addr = get_address4(host, port);
+  free(port);
+  
+  if ((addr == NULL)) {
+    return 2;
+  }
+  success_2 = bind4(fd_2, addr);
+  if ((!success_2)) {
+    free(addr);
+    
+    return 3;
+  }
   packet = constructPacket(mode, version);
   buffer = copyByte(packet);
-  temp = gwrite(fd, buffer);
-  temp = greadOffset(fd, buffer, 0);
-  return ((int) (((buffer->arr[9]) * ((char) 1000000000)) + ((buffer->arr[10]) * (((char) 1000000000) >> 32))));
+  sent = udp_send4(fd_2, buffer, (buffer->length), addr);
+  tmp_1 = newSPLArray_char( 12);
+  response = tmp_1;
+  received = udp_recv4(fd_2, response, addr);
+  closed = gclose(fd_2);
+  free(addr);
+  
+  res_1 = ((int) (((response->arr[9]) * ((char) 1000000000)) + ((response->arr[10]) * (((char) 1000000000) >> 32))));
+  free(packet);
+  
+  free(response);
+  
+  free(buffer);
+  
+  temp = gclose(fd_2);
+  return res_1;
 }
 
 SPLArray_char* constructPacket (int mode_1, int version_1) {
   SPLArray_char* packet_1;
-  SPLArray_char* tmp;
+  SPLArray_char* tmp_2;
   int i_1;
   
-  tmp = newSPLArray_char( 12);
-  packet_1 = tmp;
+  tmp_2 = newSPLArray_char( 12);
+  packet_1 = tmp_2;
   i_1 = 0;
   while (true) {
     if (!((i_1 < (packet_1->length)))) {
@@ -118,11 +192,11 @@ SPLArray_char* constructPacket (int mode_1, int version_1) {
 
 SPLArray_int* copy (SPLArray_int* a) {
   SPLArray_int* b;
-  SPLArray_int* tmp_1;
+  SPLArray_int* tmp_3;
   int i_3;
   
-  tmp_1 = newSPLArray_int( (a->length));
-  b = tmp_1;
+  tmp_3 = newSPLArray_int( (a->length));
+  b = tmp_3;
   i_3 = 0;
   while (true) {
     if (!((i_3 < (a->length)))) {
@@ -136,11 +210,11 @@ SPLArray_int* copy (SPLArray_int* a) {
 
 SPLArray_char* copyByte (SPLArray_char* a_1) {
   SPLArray_char* b_1;
-  SPLArray_char* tmp_2;
+  SPLArray_char* tmp_4;
   int i_5;
   
-  tmp_2 = newSPLArray_char( (a_1->length));
-  b_1 = tmp_2;
+  tmp_4 = newSPLArray_char( (a_1->length));
+  b_1 = tmp_4;
   i_5 = 0;
   while (true) {
     if (!((i_5 < (a_1->length)))) {
@@ -154,10 +228,10 @@ SPLArray_char* copyByte (SPLArray_char* a_1) {
 
 SPLArray_char* intToByteArray (int i_8) {
   SPLArray_char* arr;
-  SPLArray_char* tmp_3;
+  SPLArray_char* tmp_5;
   
-  tmp_3 = newSPLArray_char( 4);
-  arr = tmp_3;
+  tmp_5 = newSPLArray_char( 4);
+  arr = tmp_5;
   (arr->arr[0]) = ((char) (i_8 >> 0));
   (arr->arr[1]) = ((char) (i_8 >> 8));
   (arr->arr[2]) = ((char) (i_8 >> 16));
--- server.c
+++ server.c
@@ -4,6 +4,7 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <netinet/in.h>
 
 /*
  * Preloaded Code
@@ -57,21 +58,47 @@ SPLArray_generic* newSPLArray_generic(int size) {
   return a;
 }
 
+/*
+ * Structs
+ */
+struct SocketAddressIP4;
+struct SocketAddressIP6;
+
+typedef struct SocketAddressIP4 {
+  int sin4_addr;
+  int sin4_port;
+} SocketAddressIP4;
+
+typedef struct SocketAddressIP6 {
+  SPLArray_char* sin6_addr;
+  int sin6_flowinfo;
+  int sin6_port;
+  int sin6_scope_id;
+} SocketAddressIP6;
 
 /*
  * Procedures
  */
 int Main (SPLArray_char* args);
+bool bind4 (int fd, struct SocketAddressIP4* address);
+bool bind6 (int fd_1, struct SocketAddressIP6* address_1);
 int checkPacket (SPLArray_char* packet);
 SPLArray_char* constructPacket (SPLArray_char* req);
 SPLArray_int* copy (SPLArray_int* a);
-int gclose (int fd);
+int create_socket (int inet_type, int socket_type, int protocol);
+int gclose (int fd_3);
+struct SocketAddressIP4* get_address4 (SPLArray_char* node, SPLArray_char* service);
+struct SocketAddressIP6* get_address6 (SPLArray_char* node_1, SPLArray_char* service_1);
 int gopen (SPLArray_char* pathname, int flags);
-int gread (int fd_2, SPLArray_char* buffer);
-int greadOffset (int fd_3, SPLArray_char* buffer_1, int offset);
-int gwrite (int fd_4, SPLArray_char* buffer_2);
-SPLArray_char* intToByteArray (int i_4);
+int gread (int fd_5, SPLArray_char* buffer);
+int greadOffset (int fd_6, SPLArray_char* buffer_1, int offset);
+int gwrite (int fd_7, SPLArray_char* buffer_2);
+SPLArray_char* intToByteArray (int i_5);
 void server (SPLArray_char* host);
+int udp_recv4 (int fd_9, SPLArray_char* msg, struct SocketAddressIP4* from);
+int udp_recv6 (int fd_10, SPLArray_char* msg_1, struct SocketAddressIP6* from_1);
+int udp_send4 (int fd_11, SPLArray_char* msg_2, int len, struct SocketAddressIP4* address_4);
+int udp_send6 (int fd_12, SPLArray_char* msg_3, int len_1, struct SocketAddressIP6* address_5);
 
 int Main (SPLArray_char* args) {
   int res;
@@ -135,52 +162,101 @@ SPLArray_char* constructPacket (SPLArray_char* req) {
 SPLArray_int* copy (SPLArray_int* a) {
   SPLArray_int* b;
   SPLArray_int* tmp_1;
-  int i_1;
+  int i_2;
   
   tmp_1 = newSPLArray_int( (a->length));
   b = tmp_1;
-  i_1 = 0;
+  i_2 = 0;
   while (true) {
-    if (!((i_1 < (a->length)))) {
+    if (!((i_2 < (a->length)))) {
       break;
     }
-    (b->arr[i_1]) = (a->arr[i_1]);
-    i_1 = (i_1 + 1);
+    (b->arr[i_2]) = (a->arr[i_2]);
+    i_2 = (i_2 + 1);
   }
   return b;
 }
 
-SPLArray_char* intToByteArray (int i_4) {
+SPLArray_char* intToByteArray (int i_5) {
   SPLArray_char* arr;
   SPLArray_char* tmp_2;
   
   tmp_2 = newSPLArray_char( 4);
   arr = tmp_2;
-  (arr->arr[0]) = ((char) (i_4 >> 0));
-  (arr->arr[1]) = ((char) (i_4 >> 8));
-  (arr->arr[2]) = ((char) (i_4 >> 16));
-  (arr->arr[3]) = ((char) (i_4 >> 24));
+  (arr->arr[0]) = ((char) (i_5 >> 0));
+  (arr->arr[1]) = ((char) (i_5 >> 8));
+  (arr->arr[2]) = ((char) (i_5 >> 16));
+  (arr->arr[3]) = ((char) (i_5 >> 24));
   return arr;
 }
 
 void server (SPLArray_char* host) {
-  SPLArray_char* tmp_5;
-  int tmp_4;
+  SPLArray_char* toSend;
+  int tmp_5;
+  SPLArray_char* tmp_4;
   SPLArray_char* tmp_3;
-  int temp;
-  int flags_1;
-  int fd_5;
-  SPLArray_char* buffer_3;
+  int sent;
+  int recd;
+  SPLArray_char* port;
+  int fd_8;
+  SPLArray_char* content;
+  int closed;
+  bool bound;
+  struct SocketAddressIP4* addr;
   
-  flags_1 = 0;
-  fd_5 = gopen(host, flags_1);
-  tmp_3 = newSPLArray_char( 12);
-  buffer_3 = tmp_3;
-  temp = gread(fd_5, buffer_3);
-  tmp_4 = checkPacket(buffer_3);
-  if ((!(tmp_4 == 0))) {
-    tmp_5 = constructPacket(buffer_3);
-    temp = gwrite(fd_5, tmp_5);
+  tmp_3 = newSPLArray_char( 4);
+  port = tmp_3;
+  (port->arr[0]) = ((char) 49);
+  (port->arr[1]) = ((char) 50);
+  (port->arr[2]) = ((char) 51);
+  (port->arr[3]) = ((char) 0);
+  addr = get_address4(host, port);
+  free(port);
+  
+  if ((addr == NULL)) {
+    return;
+  }
+  fd_8 = create_socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
+  if ((fd_8 < 0)) {
+    free(addr);
+    
+    return;
+  }
+  bound = bind4(fd_8, addr);
+  if ((!bound)) {
+    free(addr);
+    
+    return;
+  }
+  tmp_4 = newSPLArray_char( 12);
+  content = tmp_4;
+  recd = udp_recv4(fd_8, content, addr);
+  if ((!(recd == 12))) {
+    free(addr);
+    
+    free(content);
+    
+    return;
+  }
+  tmp_5 = checkPacket(content);
+  if ((!(tmp_5 == 1))) {
+    free(addr);
+    
+    free(content);
+    
+    return;
+  }
+  toSend = constructPacket(content);
+  sent = udp_send4(fd_8, toSend, (toSend->length), addr);
+  closed = gclose(fd_8);
+  free(addr);
+  
+  free(content);
+  
+  free(toSend);
+  
+  if ((!(sent == (toSend->length)))) {
+    return;
   }
 }
 
