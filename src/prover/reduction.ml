d0d7eb530721de295a9c5c70e6699329302e8844
Thomas Wies <wies@cs.nyu.edu>
2016-06-16 14:56:55 -0400

    minor changes

--- src/prover/reduction.ml
+++ src/prover/reduction.ml
@@ -304,24 +304,23 @@ let add_read_write_axioms fs =
           (* f [x := d], y.(f [x := d]) -> y.f *)
           ([Match (mk_write fld1 (loc1 :: ivars1) dvar, []);
             Match (mk_read (mk_write fld1 (loc1 :: ivars1) dvar) (loc2 :: ivars2), []);
-            Match (loc1, [FilterNotNull]);
+            (*Match (loc1, [FilterNotNull]);*)
             Match (loc2, [FilterNotNull])],
            [mk_read fld1 (loc2 :: ivars2)]) ::
           (* f [x := d], y.f -> y.(f [x := d]) *)
           ([Match (mk_write fld1 (loc1 :: ivars1) dvar, []);
             Match (mk_read fld1 (loc2 :: ivars2), []);
-            Match (loc1, [FilterNotNull]);
+            (*Match (loc1, [FilterNotNull]);*)
             Match (loc2, [FilterNotNull])],
            [mk_read (mk_write fld1 (loc1 :: ivars1) dvar) (loc2 :: ivars2)]) ::
           (* Frame (x, a, f, g), y.g -> y.f *)
           ([Match (mk_frame_term set1 set2 fld1 fld2, []);
             Match (mk_read fld2 (loc1 :: ivars1), []);
-            Match (loc2, [FilterNotNull])],
+            Match (loc1, [FilterNotNull])],
            [mk_read fld1 (loc1 :: ivars1)]) ::
           (* Frame (x, a, f, g), y.f -> y.g *)
           ([Match (mk_frame_term set1 set2 fld1 fld2, []);
-            Match (mk_read fld1 (loc1 :: ivars1), []);
-            Match (loc2, [FilterNotNull])],
+            Match (mk_read fld1 (loc1 :: ivars1), [])],
            [mk_read fld2 (loc1 :: ivars1)]) ::
           propagators
       | _ -> fun propagators -> propagators)
