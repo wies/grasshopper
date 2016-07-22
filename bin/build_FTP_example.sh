966f22d27f034f5c679c7aca4650ff178e7a7afc
Damien Zufferey <zufferey@csail.mit.edu>
2016-07-22 16:28:09 -0400

    fixing small things

--- bin/build_FTP_example.sh
+++ bin/build_FTP_example.sh
@@ -13,12 +13,14 @@ mkdir -p $OUT
 
 echo compiling the C files
 
-gcc -c ${1} $LIB/console.c    -o $OUT/console.o
-gcc -c ${1} $LIB/file.c       -o $OUT/file.o
-gcc -c ${1} $LIB/socket.c     -o $OUT/socket.o
-gcc -c ${1} $LIB/makeStr.c    -o $OUT/makeStr.o
-gcc -c ${1} $OUT/ftpClient.c  -o $OUT/ftpClient.o
-gcc -c ${1} $OUT/ftpServer.c  -o $OUT/ftpServer.o
-gcc ${1} $OUT/console.o $OUT/file.o $OUT/socket.o $OUT/makeStr.o $OUT/ftpServer.o -o $OUT/ftpServer
-gcc ${1} $OUT/console.o $OUT/file.o $OUT/socket.o $OUT/makeStr.o $OUT/ftpClient.o -o $OUT/ftpClient
+opts='--std=gnu99'
+
+gcc $opts -c $LIB/console.c    -o $OUT/console.o
+gcc $opts -c $LIB/file.c       -o $OUT/file.o
+gcc $opts -c $LIB/socket.c     -o $OUT/socket.o
+gcc $opts -c $LIB/makeStr.c    -o $OUT/makeStr.o
+gcc $opts -c $OUT/ftpClient.c  -o $OUT/ftpClient.o
+gcc $opts -c $OUT/ftpServer.c  -o $OUT/ftpServer.o
+gcc $OUT/console.o $OUT/file.o $OUT/socket.o $OUT/makeStr.o $OUT/ftpServer.o -o $OUT/ftpServer
+gcc $OUT/console.o $OUT/file.o $OUT/socket.o $OUT/makeStr.o $OUT/ftpClient.o -o $OUT/ftpClient
 
--- lib/console.c
+++ lib/console.c
@@ -1,5 +1,6 @@
 #include <string.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 /*
  * Preloaded Code
