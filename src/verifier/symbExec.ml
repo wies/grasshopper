ed1cb4f341ed570714a97929dbba77f2c16b48cb
Siddharth Krishna <siddharth-krishna@users.noreply.github.com>
2017-12-12 14:22:49 -0500

    symbExec: add basic equality reasoning & tests

--- src/verifier/symbExec.ml
+++ src/verifier/symbExec.ml
@@ -6,6 +6,10 @@ open GrassUtil
 open Prog
 
 exception NotYetImplemented
+let todo () = raise NotYetImplemented
+
+
+let string_of_id_term_map sm = (IdMap.fold (fun x t str -> str ^ (Printf.sprintf "  %s -> %s" (string_of_ident x) (string_of_term t))) sm "")
 
 
 (** ----------- Symbolic state and manipulators ---------- *)
@@ -18,18 +22,20 @@ let state_empty = (mk_true, [])
 (** Convert a specification into a symbolic state. *)
 let state_of_spec_list specs : state =
   let rec state_of_sl_form (pure, spatial) = function
-    | Sl.Pure (f, _) -> smk_and [f; pure], spatial
+    | Sl.Pure (f, _) -> state_of_form (pure, spatial) f
     | Sl.Atom (p, ts, _) -> pure, (p, ts) :: spatial
     | Sl.SepOp (Sl.SepStar, f1, f2, _) ->
       let pure, spatial = state_of_sl_form (pure, spatial) f2 in
       state_of_sl_form (pure, spatial) f1
-    | Sl.SepOp (Sl.SepIncl, _, _, _) -> raise NotYetImplemented
-    | Sl.SepOp (Sl.SepPlus, _, _, _) -> raise NotYetImplemented
-    | Sl.BoolOp _ -> raise NotYetImplemented
-    | Sl.Binder _ -> raise NotYetImplemented
+    | Sl.SepOp (Sl.SepIncl, _, _, _) -> todo ()
+    | Sl.SepOp (Sl.SepPlus, _, _, _) -> todo ()
+    | Sl.BoolOp _ -> todo ()
+    | Sl.Binder _ -> todo ()
     (* Note: if you allow binders, make substitutious capture avoiding! *)
+  and state_of_form (pure, spatial) f =
+    let f = filter_annotations (fun _ -> false) f in
+    smk_and [f; pure], spatial
   in
-  let state_of_form (pure, spatial) f = smk_and [f; pure], spatial in
   List.fold_left (fun state spec ->
       match spec.spec_form with
       | SL slform -> state_of_sl_form state slform
@@ -38,11 +44,14 @@ let state_of_spec_list specs : state =
 
 let string_of_state ((pure, spatial): state) =
   let spatial =
-    List.fold_right (fun (p, ts) f -> Sl.SepOp (Sl.SepStar, Sl.Atom (p, ts, None), f, None))
-      spatial (Sl.Pure (mk_true, None))
+    match spatial with
+    | [] -> "emp"
+    | spatial ->
+      spatial
+      |> List.map (fun (p, ts) -> Sl.Atom (p, ts, None) |> Sl.string_of_form)
+      |> String.concat " * "
   in
-  Sl.SepOp (Sl.SepStar, Sl.Pure (pure, None), spatial, None)
-  |> Sl.string_of_form
+  Printf.sprintf "%s | %s" (string_of_form pure) spatial
 
 (** Substitute all variables (constants) in state [(pure, spatial)] with terms 
   according to substitution map [sm].
@@ -61,10 +70,70 @@ let print_state src_pos state =
   )
 
 
+(** ----------- Re-arrangement and normalization rules ---------- *)
+
+let find_equalities (pure: form) =
+  let rec find_eq sm = function
+    | Atom (App (Eq, [t2; (App (FreeSym id, [],  _))], _), _)
+    | Atom (App (Eq, [(App (FreeSym id, [],  _)); t2], _), _) ->
+      (* TODO do some kind of normalization to avoid checking both cases *)
+      IdMap.add id t2 sm
+    | BoolOp (And, fs) ->
+      List.fold_left find_eq sm fs
+    | _ -> sm
+  in
+  find_eq IdMap.empty pure
+
+let rec remove_trivial_equalities = function
+  | Atom (App (Eq, [t1; t2], _), _) as f -> if t1 = t2 then mk_true else f
+  | BoolOp (op, fs) -> smk_op op (List.map remove_trivial_equalities fs)
+  | Binder (b, vs, f, anns) -> Binder (b, vs, remove_trivial_equalities f, anns)
+  | f -> f
+
+
+(** ----------- Lemmas for proving entailments ---------- *)
+
+(** A lemma is of the form: pure /\ spatial |= pure /\ spatial *)
+type lemma = form * (Sl.pred_symbol * term list) list * form * (Sl.pred_symbol * term list) list
+
+let dummy_srt = FreeSrt ("TODO", 0)
+let x = Var (("x", 0), dummy_srt)
+let y = Var (("y", 0), dummy_srt)
+let nil = mk_null dummy_srt
+let lseg = ("lseg", 0)
+let next = mk_free_const dummy_srt ("next", 0)
+
+let unfolding_lemmas: lemma list = [
+  (* x != nil /\ lseg(x, nil) |= x |-> y * lseg(y, nil) *)
+  (mk_neq x nil, [(Sl.Pred lseg, [x; nil])], mk_eq (* TODO x.*)next y, [(Sl.Region, [x]); (Sl.Pred lseg, [y; nil])]);
+]
+(* TODO This is too painful. Extract from predicate instead.
+  args can be forall vars, returns can be new existentials *)
+
 (** ----------- Symbolic Execution ---------- *)
 
-let check_entailment f1 f2 =
-  raise NotYetImplemented
+let check_entailment (p1, sp1) (p2, sp2) =
+  Printf.printf "\n----Checking entailment:\n%s\n    |=\n%s\n" (string_of_state (p1, sp1)) (string_of_state (p2, sp2));
+
+  (* Find equalities in LHS and substitute in RHS *)
+  let eq_sm = find_equalities p1 in
+  Printf.printf "\nFound equalities to substitute: %s\n" (string_of_id_term_map eq_sm);
+  let (p1, sp1) = subst_state eq_sm (p1, sp1) in
+  let (p2, sp2) = subst_state eq_sm (p2, sp2) in
+  Printf.printf "\nNew RHS: %s\n" (string_of_state (p2, sp2));
+
+  (* Remove trivial equalities *)
+  let (p1, p2) = remove_trivial_equalities p1, remove_trivial_equalities p2 in
+
+  match sp2 with
+  | [] -> (* If RHS spatial is emp, then pure part must be true *)
+    if p2 = mk_true then begin
+      Printf.printf "RHS is true | emp.\n";
+      [] end
+    else
+      (* TODO call SMT solver here? :) *)
+      todo ()
+  | _ -> todo ()
 
 
 (** Symbolically execute command [comm] on state [state] and return final state. *)
@@ -81,7 +150,7 @@ let rec symb_exec state comm =
         Printf.printf "--Assignment %s := %s\n" (string_of_ident id) (string_of_term t);
         let id' = fresh_ident (name id) in
         let sm = IdMap.singleton id (mk_var_term id') in
-        print_endline (IdMap.fold (fun x t str -> str ^ (Printf.sprintf "  %s -> %s" (string_of_ident x) (string_of_term t))) sm "");
+        print_endline (string_of_id_term_map sm);
         let t' = subst_consts_term sm t in
         Printf.printf "----Substituted id' = %s, t' = %s\n" (string_of_ident id') (string_of_term t');
         let (pure, spatial) = subst_state sm state in
@@ -89,7 +158,7 @@ let rec symb_exec state comm =
       ) state
   | Seq (comms, _) ->
     List.fold_left symb_exec state comms
-  | _ -> raise NotYetImplemented
+  | _ -> todo ()
 
 
 (** Check procedure [proc] in program [prog] using symbolic execution. *)
