a794fc5777e339d6883abdbd1ae214f0f9a01af1
Siddharth Krishna <siddharth-krishna@users.noreply.github.com>
2017-12-12 17:14:48 -0500

    symbExec: extract lemmas from procedures, prettier debug

--- src/verifier/symbExec.ml
+++ src/verifier/symbExec.ml
@@ -51,9 +51,9 @@ let string_of_state ((pure, spatial): state) =
       |> List.map (fun (p, ts) -> Sl.Atom (p, ts, None) |> Sl.string_of_form)
       |> String.concat " * "
   in
-  Printf.sprintf "%s | %s" (string_of_form pure) spatial
+  Printf.sprintf "%s : %s" (string_of_form pure) spatial
 
-(** Substitute all variables (constants) in state [(pure, spatial)] with terms 
+(** Substitute all variables (encoded as constants) in state [(pure, spatial)] with terms 
   according to substitution map [sm].
   This operation is not capture avoiding. *)
 let subst_state sm ((pure, spatial): state) : state =
@@ -76,6 +76,7 @@ let find_equalities (pure: form) =
   let rec find_eq sm = function
     | Atom (App (Eq, [t2; (App (FreeSym id, [],  _))], _), _)
     | Atom (App (Eq, [(App (FreeSym id, [],  _)); t2], _), _) ->
+      (* TODO don't add equalities with field read terms! *)
       (* TODO do some kind of normalization to avoid checking both cases *)
       IdMap.add id t2 sm
     | BoolOp (And, fs) ->
@@ -93,22 +94,38 @@ let rec remove_trivial_equalities = function
 
 (** ----------- Lemmas for proving entailments ---------- *)
 
-(** A lemma is of the form: pure /\ spatial |= pure /\ spatial *)
-type lemma = form * (Sl.pred_symbol * term list) list * form * (Sl.pred_symbol * term list) list
+(** A lemma is of the form: pure /\ spatial |= pure /\ spatial.
+  Universals are represented as Var terms, and existentials as FreeSymb consts.
+*)
+type lemma = state * state
 
-let dummy_srt = FreeSrt ("TODO", 0)
-let x = Var (("x", 0), dummy_srt)
-let y = Var (("y", 0), dummy_srt)
-let nil = mk_null dummy_srt
-let lseg = ("lseg", 0)
-let next = mk_free_const dummy_srt ("next", 0)
 
-let unfolding_lemmas: lemma list = [
-  (* x != nil /\ lseg(x, nil) |= x |-> y * lseg(y, nil) *)
-  (mk_neq x nil, [(Sl.Pred lseg, [x; nil])], mk_eq (* TODO x.*)next y, [(Sl.Region, [x]); (Sl.Pred lseg, [y; nil])]);
-]
-(* TODO This is too painful. Extract from predicate instead.
-  args can be forall vars, returns can be new existentials *)
+(** Extract all lemmas (procedures named "lemma_*") from a program.
+  procedure (x1, .. xN) returns (y1, .. yM)
+    requires phi
+    ensures psi
+  is the lemma: forall x1, .. xN. phi |= exists y1, .. yM psi *)
+let extract_lemmas prog : lemma list =
+  let try_add_lemma ls proc =
+    if Str.string_match (Str.regexp "lemma_*") (proc |> name_of_proc |> fst) 0
+    then begin
+      let lhs = state_of_spec_list proc.proc_contract.contr_precond in
+      let rhs = state_of_spec_list proc.proc_contract.contr_postcond in
+      (* Take formals (x1, .. xN) and create substitution map to make them vars *)
+      let universals_sm =
+        let dummy_srt = (FreeSrt ("TODO", 0)) in  (* TODO need to find actual sort? *)
+        List.fold_left (fun sm id -> IdMap.add id (mk_var dummy_srt id) sm)
+          IdMap.empty
+          proc.proc_contract.contr_formals
+      in
+      Printf.printf "\n----Universals in lemma %s:%s\n" (proc |> name_of_proc |> fst) (string_of_id_term_map universals_sm);
+      let lhs = subst_state universals_sm lhs in
+      let rhs = subst_state universals_sm rhs in
+      (lhs, rhs) :: ls
+    end else ls
+  in
+  fold_procs try_add_lemma [] prog
+
 
 (** ----------- Symbolic Execution ---------- *)
 
@@ -138,7 +155,6 @@ let check_entailment (p1, sp1) (p2, sp2) =
 
 (** Symbolically execute command [comm] on state [state] and return final state. *)
 let rec symb_exec state comm =
-  print_state (source_pos comm) state;
   (* TODO get the type from the program somehow? *)
   let mk_var_term id = mk_free_const (FreeSrt ("TODO", 0)) id in
   match comm with
@@ -147,13 +163,12 @@ let rec symb_exec state comm =
     (* TODO simultaneous assignments can't touch heap, so do all at once *)
     List.combine ids ts
     |> List.fold_left (fun state (id, t) ->
-        Printf.printf "--Assignment %s := %s\n" (string_of_ident id) (string_of_term t);
+        Printf.printf "\nExecuting assignment: %s := %s;\n" (string_of_ident id) (string_of_term t);
         let id' = fresh_ident (name id) in
         let sm = IdMap.singleton id (mk_var_term id') in
-        print_endline (string_of_id_term_map sm);
         let t' = subst_consts_term sm t in
-        Printf.printf "----Substituted id' = %s, t' = %s\n" (string_of_ident id') (string_of_term t');
         let (pure, spatial) = subst_state sm state in
+        print_state (source_pos comm) state;
         smk_and [(mk_eq (mk_var_term id) t'); pure], spatial
       ) state
   | Seq (comms, _) ->
@@ -166,10 +181,13 @@ let check prog proc =
   Debug.info (fun () ->
       "Checking procedure " ^ string_of_ident (name_of_proc proc) ^ "...\n");
 
+  let _ = extract_lemmas prog in
+
   match proc.proc_body with
   | Some comm ->
     let precond = state_of_spec_list proc.proc_contract.contr_precond in
     let postcond = state_of_spec_list proc.proc_contract.contr_postcond in
+    print_state (comm |> source_pos |> start_pos) precond;
     let state = symb_exec precond comm in
     print_state (comm |> source_pos |> end_pos) state;
     check_entailment state postcond
