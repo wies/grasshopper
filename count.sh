b3d27e2e6470c227cd8a407e308210fd77658f58
Siddharth Krishna <siddharth-krishna@users.noreply.github.com>
2018-11-20 21:02:54 +0000

    remove more files added by mistake in 22b8fa1; fixes #37

--- count.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for f in tests/spl/include/ordered_type.spl tests/spl/reloc/*.spl 
-do
-    echo $f >> lines.txt
-    python bin/line-counter.py $f >> lines.txt
-    echo >> lines.txt
-done
