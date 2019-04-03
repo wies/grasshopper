5dc5b059000a19f3fc72c595c76018bbe42176f2
wies <wies@cs.nyu.edu>
2019-04-03 11:59:03 -0400

    optimize axiomatization of array map function

--- src/prover/axioms.ml
+++ src/prover/axioms.ml
@@ -468,9 +468,8 @@ let array_axioms elem_srt =
     mk_or [mk_eq a (mk_null (Array elem_srt)); mk_leq (mk_int 0) (mk_length a)]
   in
   let array_map_simple1 =
-    mk_or [mk_lt i (mk_int 0);
-           mk_geq i (mk_length a);
-           mk_eq (mk_read arrstate [a; i]) (mk_read (App (ArrayMap, [arrstate; a], Map ([Int], elem_srt))) [i])] 
+    smk_or ((if !Config.instantiate then [] else [mk_lt i (mk_int 0); mk_geq i (mk_length a)]) @
+           [mk_eq (mk_read arrstate [a; i]) (mk_read (App (ArrayMap, [arrstate; a], Map ([Int], elem_srt))) [i])])
   in
   let array_map_simple2 =
     mk_or [mk_and [mk_geq i (mk_int 0); mk_lt i (mk_length a)];
