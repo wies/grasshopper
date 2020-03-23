3bb0800368d97e1c02bfc8cda2c2f1080bcf62d7
Siddharth Krishna <siddharth-krishna@users.noreply.github.com>
2020-03-23 06:47:56 -0400

    xp_pldi20: don't suppress errors, print table at the end

--- bin/xp_pldi20.sh
+++ bin/xp_pldi20.sh
@@ -5,7 +5,7 @@ source bin/osx_gnu.sh
 #echo "Building Grasshopper"
 #./build.sh
 
-FILES1="flows css ccm multiset-ccm multipair-ccm give-up link"
+FILES1="flows ccm multiset-ccm multipair-ccm give-up link"
 FILES2="hashtbl-give-up"
 FILES3="hashtbl-link"
 FILES4="b+-tree"
@@ -18,6 +18,7 @@ timesfile=/tmp/times-grasshopper
 locfile=/tmp/loc-grasshopper
 timestotalfile=/tmp/times-total-grasshopper
 loctotalfile=/tmp/loc-total-grasshopper
+outputfile=/tmp/implementations-table
 
 SPLPATH=tests/spl/flows
 
@@ -25,24 +26,29 @@ run()
 {
     name="${1}"
     tabs=$((2 - ${#name} / 8))
-    echo -n "$name"
-    perl -E "print \"\t\" x $tabs"
+    echo -n "$name" >> $outputfile
+    perl -E "print \"\t\" x $tabs" >> $outputfile
     shift
     rm -f $timesfile $locfile
     for f in $@ ; do
         #echo "processessing $f"
         python bin/line-counter.py $SPLPATH/$f.spl >> $locfile
-        { TIMEFORMAT=%3R; time ./grasshopper.native $SPLPATH/$f.spl -module $f ; } 2>> $timesfile
+        echo "./grasshopper.native $SPLPATH/$f.spl -module $f"
+        { TIMEFORMAT=%3R; time ./grasshopper.native $SPLPATH/$f.spl -module $f 2>&1 ; } 2>> $timesfile
+        retcode=$?
+        if [ $retcode -ne 0 ]; then
+            echo -e "\nGrasshopper exited with errors on file $f.spl.\n"
+        fi
     done
-    awk -F "\t" '{specs+=$1; progs+=$2; total+=$3} END{printf("\t& %d\t& %d\t& %d", progs, specs, total);}' $locfile
+    awk -F "\t" '{specs+=$1; progs+=$2; total+=$3} END{printf("\t& %d\t& %d\t& %d", progs, specs, total);}' $locfile >> $outputfile
     awk -F "\t" '{specs+=$1; progs+=$2; total+=$3} END{printf("%d\t%d\t%d\n", progs, specs, total);}' $locfile >> $loctotalfile
-    awk '{sum+=$1;} END{printf("\t& %d\n", int(sum+0.5));}' $timesfile
+    awk '{sum+=$1;} END{printf("\t& %d\n", int(sum+0.5));}' $timesfile >> $outputfile
     awk '{sum+=$1;} END{printf("%d\n", int(sum+0.5));}' $timesfile >> $timestotalfile
-
 }
 
-rm -f $loctotalfile $timestotalfile
-echo -e "; Module\t\t& Code\t& Proof\t& Total\t& Time"
+rm -f $loctotalfile $timestotalfile $outputfile
+
+echo -e "; Module\t\t& Code\t& Proof\t& Total\t& Time" >> $outputfile
 run "Flow library" $FILES1
 run "Array library" $FILES8
 run "B+ tree" $FILES4
@@ -52,6 +58,9 @@ run "B-tree (full split)" $FILES7
 run "Hash table (link)" $FILES2
 run "Hash table (give-up)" $FILES3
 
-echo -n -e "Total\t\t"
-awk -F "\t" '{progs+=$1; specs+=$2; total+=$3} END{printf("\t& %d\t& %d\t& %d", progs, specs, total);}' $loctotalfile
-awk '{sum+=$1;} END{printf("\t& %d\n", int(sum+0.5));}' $timestotalfile
+echo -n -e "Total\t\t" >> $outputfile
+awk -F "\t" '{progs+=$1; specs+=$2; total+=$3} END{printf("\t& %d\t& %d\t& %d", progs, specs, total);}' $loctotalfile >> $outputfile
+awk '{sum+=$1;} END{printf("\t& %d\n", int(sum+0.5));}' $timestotalfile >> $outputfile
+
+echo ""
+cat $outputfile
